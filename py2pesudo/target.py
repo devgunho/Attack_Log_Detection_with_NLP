4c9974bf98f1bcd44af06bcbcaea58c984d55a10
Gunho Park <devgun@kakao.com>
2021-12-19 04:56:47 +0900

    add: attention model temp pseudo code

--- py2pesudo/target.py
+++ py2pesudo/target.py
@@ -1,56 +1,131 @@
-for i in range(0, len(X_train)):
-    y_train[i] = "\t" + y_train[i] + "\n"
+class AttentionLayer(Layer):
+    """
+    This class implements Bahdanau attention (https://arxiv.org/pdf/1409.0473.pdf).
+    There are three sets of weights introduced W_a, U_a, and V_a
+    """
 
-    for char in X_train[i]:
-        if char not in input_characters:
-            input_characters.add(char)
-    for char in y_train[i]:
-        if char not in target_characters:
-            target_characters.add(char)
+    def __init__(self, **kwargs):
+        super(AttentionLayer, self).__init__(**kwargs)
 
-input_characters = sorted(list(input_characters))
-target_characters = sorted(list(target_characters))
-num_encoder_tokens = len(input_characters)
-num_decoder_tokens = len(target_characters)
-max_X_len = max([len(txt) for txt in X_train])
-max_y_len = max([len(txt) for txt in y_train])
+    def build(self, input_shape):
+        assert isinstance(input_shape, list)
+        # Create a trainable weight variable for this layer.
 
+        self.W_a = self.add_weight(
+            name="W_a",
+            shape=tf.TensorShape((input_shape[0][2], input_shape[0][2])),
+            initializer="uniform",
+            trainable=True,
+        )
+        self.U_a = self.add_weight(
+            name="U_a",
+            shape=tf.TensorShape((input_shape[1][2], input_shape[0][2])),
+            initializer="uniform",
+            trainable=True,
+        )
+        self.V_a = self.add_weight(
+            name="V_a",
+            shape=tf.TensorShape((input_shape[0][2], 1)),
+            initializer="uniform",
+            trainable=True,
+        )
 
-if max_X_len > max_encoder_seq_length:
-    max_encoder_seq_length = max_X_len
+        super(AttentionLayer, self).build(
+            input_shape
+        )  # Be sure to call this at the end
 
-if max_y_len > max_decoder_seq_length:
-    max_decoder_seq_length = max_y_len
+    def call(self, inputs, verbose=False):
+        """
+        inputs: [encoder_output_sequence, decoder_output_sequence]
+        """
+        assert type(inputs) == list
+        encoder_out_seq, decoder_out_seq = inputs
+        if verbose:
+            print("encoder_out_seq>", encoder_out_seq.shape)
+            print("decoder_out_seq>", decoder_out_seq.shape)
 
-print(target_characters)
+        def energy_step(inputs, states):
+            """Step function for computing energy for a single decoder state
+            inputs: (batchsize * 1 * de_in_dim)
+            states: (batchsize * 1 * de_latent_dim)
+            """
 
-input_token_index = dict([(char, i) for i, char in enumerate(input_characters)])
-target_token_index = dict([(char, i) for i, char in enumerate(target_characters)])
+            assert_msg = "States must be an iterable. Got {} of type {}".format(
+                states, type(states)
+            )
+            assert isinstance(states, list) or isinstance(states, tuple), assert_msg
 
-encoder_input_data = np.zeros(
-    (len(X_train), max_encoder_seq_length, num_encoder_tokens), dtype="float32"
-)
-decoder_input_data = np.zeros(
-    (len(X_train), max_decoder_seq_length, num_decoder_tokens), dtype="float32"
-)
-decoder_target_data = np.zeros(
-    (len(X_train), max_decoder_seq_length, num_decoder_tokens), dtype="float32"
-)
+            """ Some parameters required for shaping tensors"""
+            en_seq_len, en_hidden = encoder_out_seq.shape[1], encoder_out_seq.shape[2]
+            de_hidden = inputs.shape[-1]
 
-np.set_printoptions(threshold=10)
+            """ Computing S.Wa where S=[s0, s1, ..., si]"""
+            # <= batch size * en_seq_len * latent_dim
+            W_a_dot_s = K.dot(encoder_out_seq, self.W_a)
 
-for i, (input_text, target_text) in enumerate(zip(X_train, y_train)):
-    for t, char in enumerate(input_text):
-        encoder_input_data[i, t, input_token_index[char]] = 1.0
-    encoder_input_data[i, t + 1 :, input_token_index[" "]] = 1.0
-    for t, char in enumerate(target_text):
-        # decoder_target_data is ahead of decoder_input_data by one timestep
-        decoder_input_data[i, t, target_token_index[char]] = 1.0
-        if t > 0:
-            # decoder_target_data will be ahead by one timestep
-            # and will not include the start character.
-            decoder_target_data[i, t - 1, target_token_index[char]] = 1.0
-    decoder_input_data[i, t + 1 :, target_token_index[" "]] = 1.0
-    decoder_target_data[i, t:, target_token_index[" "]] = 1.0
+            """ Computing hj.Ua """
+            U_a_dot_h = K.expand_dims(
+                K.dot(inputs, self.U_a), 1
+            )  # <= batch_size, 1, latent_dim
+            if verbose:
+                print("Ua.h>", U_a_dot_h.shape)
 
-encoder_input_data[0]
+            """ tanh(S.Wa + hj.Ua) """
+            # <= batch_size*en_seq_len, latent_dim
+            Ws_plus_Uh = K.tanh(W_a_dot_s + U_a_dot_h)
+            if verbose:
+                print("Ws+Uh>", Ws_plus_Uh.shape)
+
+            """ softmax(va.tanh(S.Wa + hj.Ua)) """
+            # <= batch_size, en_seq_len
+            e_i = K.squeeze(K.dot(Ws_plus_Uh, self.V_a), axis=-1)
+            # <= batch_size, en_seq_len
+            e_i = K.softmax(e_i)
+
+            if verbose:
+                print("ei>", e_i.shape)
+
+            return e_i, [e_i]
+
+        def context_step(inputs, states):
+            """ Step function for computing ci using ei """
+
+            assert_msg = "States must be an iterable. Got {} of type {}".format(
+                states, type(states)
+            )
+            assert isinstance(states, list) or isinstance(states, tuple), assert_msg
+
+            # <= batch_size, hidden_size
+            c_i = K.sum(encoder_out_seq * K.expand_dims(inputs, -1), axis=1)
+            if verbose:
+                print("ci>", c_i.shape)
+            return c_i, [c_i]
+
+        fake_state_c = K.sum(encoder_out_seq, axis=1)
+        fake_state_e = K.sum(
+            encoder_out_seq, axis=2
+        )  # <= (batch_size, enc_seq_len, latent_dim
+
+        """ Computing energy outputs """
+        # e_outputs => (batch_size, de_seq_len, en_seq_len)
+        last_out, e_outputs, _ = K.rnn(
+            energy_step,
+            decoder_out_seq,
+            [fake_state_e],
+        )
+
+        """ Computing context vectors """
+        last_out, c_outputs, _ = K.rnn(
+            context_step,
+            e_outputs,
+            [fake_state_c],
+        )
+
+        return c_outputs, e_outputs
+
+    def compute_output_shape(self, input_shape):
+        """ Outputs produced by the layer """
+        return [
+            tf.TensorShape((input_shape[1][0], input_shape[1][1], input_shape[1][2])),
+            tf.TensorShape((input_shape[1][0], input_shape[1][1], input_shape[0][1])),
+        ]
